5b98df4b6412049474322b213c1b181fa9bdeef3
Bean <bean1730137@gmail.com>
2025-04-07 22:20:10 +0800

    feat: react url

--- frontend/components/OCIDProvider.tsx
+++ frontend/components/OCIDProvider.tsx
@@ -8,7 +8,7 @@ interface OCIDProviderProps {
 }
 
 const opts = {
-  redirectUri: "http://localhost:3000/redirect",
+  redirectUri: "https://brain-mining.vercel.app/redirect",
   referralCode: "PARTNER6",
 };
 
